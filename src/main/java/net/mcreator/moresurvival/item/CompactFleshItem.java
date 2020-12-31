94de12175ead772f1bd082107c5cc6cc4fe57294
DPadderz <miffs@LAPTOP-EUR7C0LD>
2020-12-31 11:10:51 +0000

    Added biome: Inverted Lands.

---
+++ src/main/java/net/mcreator/moresurvival/item/CompactFleshItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.moresurvival.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.moresurvival.MoreSurvivalModElements;
+
+@MoreSurvivalModElements.ModElement.Tag
+public class CompactFleshItem extends MoreSurvivalModElements.ModElement {
+	@ObjectHolder("more_survival:compact_flesh")
+	public static final Item block = null;
+	public CompactFleshItem(MoreSurvivalModElements instance) {
+		super(instance, 13);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("compact_flesh");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
